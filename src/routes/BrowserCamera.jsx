99150c14addafe61edadb253a1bfd9dfdd00d4b1
mottameister <murilo.m.fernandes@hotmail.com>
2026-05-11 23:44:11 -0400

    Square browser camera frame

--- src/routes/BrowserCamera.jsx
+++ src/routes/BrowserCamera.jsx
@@ -11,10 +11,6 @@ export default function BrowserCamera() {
       >
         <div className="browser-camera-frame__glow" />
         <div className="browser-camera-frame__edge" />
-        <div className="browser-camera-frame__corner browser-camera-frame__corner--tl" />
-        <div className="browser-camera-frame__corner browser-camera-frame__corner--tr" />
-        <div className="browser-camera-frame__corner browser-camera-frame__corner--bl" />
-        <div className="browser-camera-frame__corner browser-camera-frame__corner--br" />
       </motion.div>
     </main>
   );
